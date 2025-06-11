04ded3a4d965fa4551982b44300d0f78824b8d35
Matthew Ham <hammatthew70@gmail.com>
2025-06-10 23:08:42 -0400

    updated home page to make use of navbar and ensured it could navigate properly

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,19 @@
 import { StyleSheet, Text, View, Image } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../assets/favicon.png'
-import { Link } from 'expo-router'
+import { Link, useLocalSearchParams } from 'expo-router'
 import { Context } from '../context'
 import { User, City} from '../types'
 import Navbar from "../components/navbar"
+import { fakeCities } from '../cities'
 
 const index = () => {
+  //overall app state -------------------------------------------------
   const [user, setUser] = useState<User | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [isLocationEnabled, setIsLocationEnabled] = useState<boolean>(false);
   const [currentCity, setCurrentCity] = useState<City | null>(null);
-  const [savedCities, setSavedCities] = useState<City[]>([]);
+  const [savedCities, setSavedCities] = useState<City[]>(fakeCities);
 
   const contextValue = {
     user: user,
@@ -27,15 +29,39 @@ const index = () => {
     setSavedCities: setSavedCities
   }
 
+  //home page state-----------------------------------------------
+  const params = useLocalSearchParams<{city_id: string;}>()
+  
+  const [city, setCity] = useState<City | undefined>()
+  function getCity(id : string): City | void{
+    for(let i = 0; i < fakeCities.length; i++){
+      if(fakeCities[i].id == id){
+        return fakeCities[i]
+      }
+    }
+    console.log(`error: city with id ${id} not found`)
+  }
+
+  useEffect(() => {
+    const c = getCity(params.city_id)
+    if(c){ setCity(c) }
+  }, [params])
+
   return (
     <Context.Provider value ={contextValue}>
     <View style={styles.container}>
-      <Image source={logo}/>
-      <Text style={styles.title}>Ether Status</Text>
-      <View style={styles.card}>
-        <Text>I am Card</Text>
-      </View>
-      <Link href="./search">search page</Link>
+      {city ? (
+        <Text>{city.name}</Text>
+      ) :(
+        <View>
+        <Image source={logo}/>
+        <Text style={styles.title}>Ether Status</Text>
+        <View style={styles.card}>
+          <Text>I am Card</Text>
+        </View>
+        <Link href="./search">search page</Link>
+        </View>
+      )}
       <Navbar/>
     </View>
     </Context.Provider>
