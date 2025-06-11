19fa5fdc039baf1e4663a7a1bfba13399372f776
Matthew Ham <hammatthew70@gmail.com>
2025-06-10 23:09:07 -0400

    edits to how navbar navigateds

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import { Context, useAppContext } from '../context'
 const Navbar = () => {
   const router = useRouter()
   const goToPage = (city_id: string) => {
-    router.push(`/${city_id}`);
+    router.push(`/?city_id=${city_id}`);
   }
   const context = useAppContext()
   const city_qaunt = context.saved_cities.length
@@ -16,7 +16,7 @@ const Navbar = () => {
       <Text>hello the navbar has context and can see {city_qaunt} amount of cities</Text>
       {context.location_enabled && (<Button title='curr_location'/>)}
       {Array.from({ length: city_qaunt}, (_, index) => index + 1).map(city_num => (
-        <Button title='city' onPress={() => goToPage(context.saved_cities[city_num].id)}/>
+        <Button title='city' onPress={() => goToPage(context.saved_cities[city_num - 1].id)}/>
       ))}
     </View>
   )
