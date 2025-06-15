f0bd9ac693b75f167b364c52ff0a47d9603c56aa
Matthew Ham <hammatthew70@gmail.com>
2025-06-15 00:12:07 -0400

    trying to establish connectio with backend using search function and  only using weather data for now

--- app/search.tsx
+++ app/search.tsx
@@ -1,21 +1,30 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import Place from '../components/place'
-import { Link, useRouter } from 'expo-router'
-import { Context, useAppContext } from '../context'
+import { useRouter } from 'expo-router'
+import { useAppContext } from '../context'
+import Searchbar from '../components/searchbar'
 
 const search = () => {
   const router = useRouter()
   const goToCityPage = (city_id: string) => {
     router.push(`/?city_id=${city_id}`)
   }
-
+  //app context
   const context = useAppContext()
   const city_qaunt = context.saved_cities.length
+
+  //search page context
+  const [query, setQuery] = useState('')
     
   return (
     <View>
-      <Text>search</Text>
+      <Text>search page</Text>
+      <Searchbar 
+        query={query}
+        onChangeQuery={setQuery}
+        onSubmit={}
+        />
       <Text>Saved Cities</Text>
       {Array.from({ length: city_qaunt}, (_, index) => index + 1).map(city_num => (
         <Button title='city' onPress={() => goToCityPage(context.saved_cities[city_num - 1].id)}/>
--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TextInput, View, Button } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
 
-const searchbar = () => {
+const Searchbar = (
+ query: string,
+ onChangeQuery: (text: string) => void,
+ onSubmit: () => void) => {
   return (
-    <View>
-      <Text>searchbar</Text>
+    <View style={styles.container}>
+      <Ionicons name="search" style={styles.icon}/>
+      <TextInput
+        style={styles.input}
+        placeholder={"city name"}
+        value={query}
+        onChangeText={onChangeQuery}
+        placeholderTextColor="#999"
+        returnKeyType="search"
+        clearButtonMode="while-editing"
+      />
+      <Button title ='Submit' onPress={onSubmit}/>
     </View>
   )
 }
 
-export default searchbar
+export default Searchbar
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    alignItems: 'center',
+    margin: 10,
+  },
+  icon: {
+    marginRight: 8,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+  },
+})
