403299f53a2cf94fa70d47db6c4832d6ac80d6be
Matthew Ham <hammatthew70@gmail.com>
2025-08-06 23:06:45 -0400

    using expo location instead of geolocation browser api so user location works on more than just web

--- services/user_location.ts
+++ services/user_location.ts
@@ -1,9 +1,9 @@
 import { City } from '../types'
+import * as Location from 'expo-location'
 
 async function get_user_location(): Promise<City | undefined> {
   const user_coords = await get_user_coords()
   const post_data = { user_coords }
-
 
   const res = await fetch(`https://ether-stat-backend.vercel.app/search/uloc`,{
     method: 'POST',
@@ -14,18 +14,15 @@ async function get_user_location(): Promise<City | undefined> {
   return res.json()
 }
 
-function get_user_coords(){
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const {latitude, longitude} = position.coords
-            resolve({latitude, longitude})
-        },
-        (error) => {
-            reject(error)
-        }
-        )
-    })
+async function get_user_coords() {
+  const { status } = await Location.requestForegroundPermissionsAsync()
+  if (status !== 'granted') {
+    throw new Error('Permission to access location was denied')
+  }
+
+  const location = await Location.getCurrentPositionAsync({})
+  const { latitude, longitude } = location.coords
+  return { latitude, longitude }
 }
 
 export default get_user_location
